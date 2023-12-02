6e5d4540bdf69fd82a634084714e5e99da6af2e8
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2023-12-03 00:54:41 +0100

    feat(command): added playtime command

---
+++ src/main/java/io/github/Alathra/AlathranCommands/commands/CommandPlaytime.java
@@ -0,0 +1,31 @@
+package io.github.Alathra.AlathranCommands.commands;
+
+import com.github.milkdrinkers.colorparser.ColorParser;
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.PlayerArgument;
+import dev.jorel.commandapi.executors.CommandArguments;
+import io.github.Alathra.AlathranCommands.AlathranCommands;
+import io.github.Alathra.AlathranCommands.utils.MiscCfg;
+import io.github.Alathra.AlathranCommands.utils.PlaytimeChecker;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+public class CommandPlaytime {
+
+    public CommandPlaytime(AlathranCommands pl) {
+        new CommandAPICommand("playtime")
+            .withPermission("alathrancommands.playtime")
+            .withOptionalArguments(new PlayerArgument("checkedPlayer"))
+            .executesPlayer((Player p, CommandArguments args) -> {
+                if (args.get("checkedPlayer") == null) {
+                    p.sendMessage(ColorParser.of(MiscCfg.get().getString("Playtime.Messages.Check-self")).parseMinimessagePlaceholder("playtime", PlaytimeChecker.playtimeString(p)).build());
+                } else {
+                    OfflinePlayer offlinePlayer = (OfflinePlayer) args.get("checkedPlayer");
+                    p.sendMessage(ColorParser.of(MiscCfg.get().getString("Playtime.Messages.Check-other")).parseMinimessagePlaceholder("player", offlinePlayer.getName()).parseMinimessagePlaceholder("playtime", PlaytimeChecker.playtimeString(offlinePlayer)).build());
+                }
+        })
+            .register();
+    }
+
+}
--- src/main/java/io/github/Alathra/AlathranCommands/utils/PlaytimeChecker.java
+++ src/main/java/io/github/Alathra/AlathranCommands/utils/PlaytimeChecker.java
@@ -1,7 +1,10 @@
 package io.github.Alathra.AlathranCommands.utils;
 
+import org.bukkit.OfflinePlayer;
 import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
+
+import java.util.concurrent.TimeUnit;
 
 public class PlaytimeChecker {
 
@@ -14,4 +17,24 @@ public class PlaytimeChecker {
         return playtimeSeconds > cfgPlaytime;
     }
 
+    public static String playtimeString(OfflinePlayer offlinePlayer) {
+        int playtime = offlinePlayer.getStatistic(Statistic.PLAY_ONE_MINUTE);
+        int playtimeSeconds = playtime / 20;
+
+        int days = (int) TimeUnit.SECONDS.toDays(playtimeSeconds);
+        long hours = TimeUnit.SECONDS.toHours(playtimeSeconds) - (days * 24);
+        long minutes = TimeUnit.SECONDS.toMinutes(playtimeSeconds) - (TimeUnit.SECONDS.toHours(playtimeSeconds) * 60);
+        long seconds = TimeUnit.SECONDS.toSeconds(playtimeSeconds) - (TimeUnit.SECONDS.toMinutes(playtimeSeconds) * 60);
+
+        if (days < 1) {
+            if (hours < 1) {
+                if (minutes < 1) {
+                    return seconds + " seconds.";
+                }
+                return minutes + " minutes and " + seconds + " seconds";
+            }
+            return hours + " hours, " + minutes + " minutes and " + seconds + " seconds";
+        }
+        return days + " days, " + hours + " hours, " + minutes + " minutes and " + seconds + " seconds";
+    }
 }
